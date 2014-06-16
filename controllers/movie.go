449fa6588853221bbe9fa051b1f42a47f22aaa9e
0x43 <wxfans@gmail.com>
2014-06-16 05:33:43 +0000

    change model

--- controllers/movie.go
+++ controllers/movie.go
@@ -2,7 +2,7 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
-    "moviego/models"
+	"moviego/models"
 )
 
 type MovieController struct {
@@ -10,56 +10,55 @@ type MovieController struct {
 }
 
 func (this *MovieController) Get() {
-    
-    q := this.GetString("q")
-    if q == ""{
-    	this.Ctx.WriteString("parmas q is empty")
-    	return
-    }
-    
 
-     /*type User struct {
-        Id_       bson.ObjectId `bson:"_id"`
-        Name      string        `bson:"name"`
-        Age       int           `bson:"age"`
-        JoinedAt   time.Time     `bson:"joined_at"`
-        Interests []string      `bson:"interests"`
-    }
-   
-    err = c.Insert(&User{
-    Id_:       bson.NewObjectId(),
-    Name:      "Jimmy Kuu",
-    Age:       33,
-    JoinedAt:  time.Now(),
-    Interests: []string{"Develop", "Movie"},
-    })
+	q := this.GetString("q")
+	if q == "" {
+		this.Ctx.WriteString("parmas q is empty")
+		return
+	}
 
-    if err != nil {
-        panic(err)
-    }
+	/*type User struct {
+	      Id_       bson.ObjectId `bson:"_id"`
+	      Name      string        `bson:"name"`
+	      Age       int           `bson:"age"`
+	      JoinedAt   time.Time     `bson:"joined_at"`
+	      Interests []string      `bson:"interests"`
+	  }
 
-    err = c.Insert(&User{
-        Id_:       bson.NewObjectId(),
-        Name:      "Tracy Yu",
-        Age:       31,
-        JoinedAt:  time.Now(),
-        Interests: []string{"Shoping", "TV"},
-    })
+	  err = c.Insert(&User{
+	  Id_:       bson.NewObjectId(),
+	  Name:      "Jimmy Kuu",
+	  Age:       33,
+	  JoinedAt:  time.Now(),
+	  Interests: []string{"Develop", "Movie"},
+	  })
 
-    if err != nil {
-        panic(err)
-    }
-    */
-    /* var users []User
-   // c.Find(bson.M{"Name": "Jimmy Kuu"}).All(&users)
-    c.Find(bson.M{"name": bson.RegEx{"Yu", ""}}).All(&users)
-    this.Data["json"] = users
-    this.ServeJson()
-    fmt.Println("here")
-   */
-    var movies[]models.Movie
+	  if err != nil {
+	      panic(err)
+	  }
 
-    movies = models.SearchMovie(q)
+	  err = c.Insert(&User{
+	      Id_:       bson.NewObjectId(),
+	      Name:      "Tracy Yu",
+	      Age:       31,
+	      JoinedAt:  time.Now(),
+	      Interests: []string{"Shoping", "TV"},
+	  })
+
+	  if err != nil {
+	      panic(err)
+	  }
+	*/
+	/* var users []User
+	   // c.Find(bson.M{"Name": "Jimmy Kuu"}).All(&users)
+	    c.Find(bson.M{"name": bson.RegEx{"Yu", ""}}).All(&users)
+	    this.Data["json"] = users
+	    this.ServeJson()
+	    fmt.Println("here")
+	*/
+	var movies []models.Movie
+
+	movies = models.SearchMovie(q)
 	this.Data["json"] = movies
 	this.ServeJson()
 }
--- models/models.go
+++ models/models.go
@@ -1,42 +1,56 @@
 package models
 
 import (
-   "github.com/astaxie/beego"
-   "github.com/sunfmin/mgodb"
-   "labix.org/v2/mgo"
-   "labix.org/v2/mgo/bson"
+	//"errors"
+	//"fmt"
+	"github.com/astaxie/beego"
+	"github.com/sunfmin/mgodb"
+	"labix.org/v2/mgo"
+	"labix.org/v2/mgo/bson"
 )
- 
-type Movie struct{
-    Id_     bson.ObjectId `bson:"_id"`
-    Alt     string   `bson:"alt"`
-    Casts   []string `bson:"casts"`
-    Countries   []string `bson:"countries"`
-    Db_id   string     `bson:"db_id"`
-    Directors   []string `bson:"directors"`
-    Download   []struct{
-        Href string `bson:"href"`
-        Name string `bson:"name"`
-        Format string `bson:"format"`
-        Size string `bson:"size"`
-        } `bson:"download"`
-    Genres   []string `bson:"genres"`
-    Id   int `bson:"id"`
-    Image   string `bson:"image"`
-    Rating   float64 `bson:"rating"`
-    Summary   string   `bson:"summary"`
-    Title   string   `bson:"title"`
-    Year   string   `bson:"year"`
+
+type Movie struct {
+	Id_       bson.ObjectId `bson:"_id"`
+	Alt       string        `bson:"alt"`
+	Casts     []string      `bson:"casts"`
+	Countries []string      `bson:"countries"`
+	Db_id     string        `bson:"db_id"`
+	Directors []string      `bson:"directors"`
+	Download  []struct {
+		Href   string `bson:"href"`
+		Name   string `bson:"name"`
+		Format string `bson:"format"`
+		Size   string `bson:"size"`
+	} `bson:"download"`
+	Genres  []string `bson:"genres"`
+	Id      int      `bson:"id"`
+	Image   string   `bson:"image"`
+	Rating  float64  `bson:"rating"`
+	Summary string   `bson:"summary"`
+	Title   string   `bson:"title"`
+	Year    string   `bson:"year"`
 }
 
-var db *mgodb.Database
- 
-func SearchMovie(keyword string) (movies []Movie){
-    dbHost := beego.AppConfig.String("mongoHost")
-    dbName := beego.AppConfig.String("mongoDb")
-    db := mgodb.NewDatabase(dbHost, dbName)
+var (
+	db *mgodb.Database
+)
+
+func init() {
+	NewDb()
+}
+
+func NewDb() {
+	dbHost := beego.AppConfig.String("mongoHost")
+	dbName := beego.AppConfig.String("mongoDb")
+
+	db = mgodb.NewDatabase(dbHost, dbName)
+
+	return
+}
+
+func SearchMovie(keyword string) (movies []Movie) {
 	db.CollectionDo("movie", func(c *mgo.Collection) {
-        c.Find(bson.M{"title": bson.RegEx{keyword, ""}}).All(&movies)
-    })
+		c.Find(bson.M{"title": bson.RegEx{keyword, ""}}).All(&movies)
+	})
 	return movies
 }
